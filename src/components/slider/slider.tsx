37ae9c8b1b95409ed7e6e226ad92a7eb87d0ad77
Gabriel Brito <67193804+gabrieldiasbrito@users.noreply.github.com>
2024-01-17 13:02:03 -0300

    update slider
    Finalizado slider e página home

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -17,17 +17,21 @@ export default function Slider(){
             modules={[Pagination]}
         >
             <SwiperSlide >
-                <img className="slide" src="https://swiperjs.com/demos/images/nature-1.jpg" />
-            </SwiperSlide>
-            <SwiperSlide >
-                <img className="slide" src="https://swiperjs.com/demos/images/nature-1.jpg" />
-            </SwiperSlide>
-            <SwiperSlide >
-                <img className="slide" src="https://swiperjs.com/demos/images/nature-1.jpg" />
-            </SwiperSlide>
-            <SwiperSlide >
-                <img className="slide" src="https://swiperjs.com/demos/images/nature-1.jpg" />
-            </SwiperSlide>
+                <div className="slide">
+                    <div>
+                        <h1>TÊNIS CAMPUS 00S</h1>
+                        <button>Detalhes</button>
+                    </div>
+                    <video loop autoPlay muted>
+                        <source src="https://assets.adidas.com/videos/ar_1,w_720,c_fill,q_auto,f_auto/f9aee4ef5561438a9fb6cd365b225047_d98c/Tenis_Campus_00s_Preto_IF8768_video.mp4"/>
+                    </video>
+                    <div> 
+                        <h3>INSPIRADO NA CENA DO SKATE DOS ANOS 2000, MAS ATUALIZADO COM DETALHES SOFISTICADOS</h3>
+                        <br />
+                        <p>Encare o dia a dia com todo o conforto e o estilo proporcionados por este tênis adidas. Versão moderna de um ícone dos anos 80, ele inclui o renomado cabedal de camurça, atualizado com detalhes em couro fosco para aumentar a profundidade. As proporções menores, mas retrô, gera um visual futurista que também honra o passado.</p>
+                    </div>
+                 </div>
+                </SwiperSlide>
         </Swiper>
     )
 }
